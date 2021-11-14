e4f96ebbe2e6d592667612ff9e74b896425673e3
kvnngyn1 <knguyen1094@gmail.com>
2021-11-14 18:30:37 -0500

    finishing touches

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -9,13 +9,12 @@ import json
 # TEST ROUTE
 @api.route('/poster', methods=['POST'])
 def get_poster():
-    
     """
     [POST] /api/poster
     """
     data = json.loads(request.data.decode('utf-8'))
     u = User.query.filter_by(email=data['user_email']).first()
-    print(u)
+    # print(u)
     # posts = [p.to_dict() for p in Post.query.all()]
     return jsonify([p.to_dict() for p in u.posts.all()])
     # return jsonify({posts})
